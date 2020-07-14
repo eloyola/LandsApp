7cb35736c45256951111fef6f85b348d1e3174c2
Enrique <enrique.loyola@gmail.com>
2020-07-14 08:30:30 +0100

    initial structure

---
+++ Lands/Lands/Infrastucture/InstantLocator.cs
@@ -0,0 +1,12 @@
+﻿using Lands.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lands.Infrastucture
+{
+    public class InstantLocator
+    {
+        public MainViewModel Main { get; set; }
+    }
+}
