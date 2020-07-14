114b9fae8b54ecbd41f3d03af0d6dee696403132
Enrique <enrique.loyola@gmail.com>
2020-07-14 19:52:22 +0100

    Icommand on login

--- Lands/Lands/ViewModels/LoginViewModel.cs
+++ Lands/Lands/ViewModels/LoginViewModel.cs
@@ -1,9 +1,11 @@
 ﻿namespace Lands.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
     using System;
     using System.Collections.Generic;
     using System.Text;
     using System.Windows.Input;
+    using Xamarin.Forms;
 
     public class LoginViewModel
     {
@@ -15,7 +17,33 @@
 
         public bool IsRemembered { get; set; }
 
-        public ICommand LoginCommand { get; set; }
+        public ICommand LoginCommand {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error", 
+                    "you must enter an email", 
+                    "Acept");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "you must enter a password",
+                    "Acept");
+                return;
+            }
+        }
 
         public LoginViewModel()
         {
