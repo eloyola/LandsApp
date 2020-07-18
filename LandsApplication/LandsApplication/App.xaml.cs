b1fbf0e8c982b9ee1434f1e6bc16ca9a940c4de4
Enrique <enrique.loyola@gmail.com>
2020-07-18 10:16:19 +0100

    borders tab

---
+++ LandsApplication/LandsApplication/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace LandsApplication
+{
+    using LandsApplication.Views;
+    using System;
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            this.MainPage = new NavigationPage(new LoginPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ LandsApplication/LandsApplication/ViewModels/LandViewModel.cs
@@ -0,0 +1,55 @@
+﻿namespace LandsApplication.ViewModels
+{
+    using LandsApplication.Models;
+    using System;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+
+    public class LandViewModel : BaseViewModel
+    {
+        private ObservableCollection<Border> borders;
+
+        #region Propperties
+        public Land Land
+        {
+            get;
+            set;
+        }
+        public ObservableCollection<Border> Borders
+        {
+            get => borders;
+            set
+            {
+                SetValue(ref this.borders, value);
+            }
+        }
+        #endregion
+
+        #region Constructors
+        public LandViewModel(Land land)
+        {
+            this.Land = land;
+            LoadBorders();
+        }
+        #endregion
+
+        private void LoadBorders()
+        {
+            this.borders = new ObservableCollection<Border>();
+            foreach (var item in this.Land.Borders)
+            {
+                var land = MainViewModel.GetInstance().LandsList
+                    .Where(p => p.Alpha3Code == item)
+                    .FirstOrDefault();
+                if (land != null)
+                {
+                    this.Borders.Add(new Border
+                    {
+                        Code = land.Alpha3Code,
+                        Name = land.Name
+                    });
+                }
+            }
+        }
+    }
+}
