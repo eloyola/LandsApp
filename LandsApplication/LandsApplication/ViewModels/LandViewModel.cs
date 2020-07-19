a751d0878dd8db52d8e1ca15d506e15d143a9c53
Enrique <enrique.loyola@gmail.com>
2020-07-19 10:06:26 +0100

    tabbed finished

--- LandsApplication/LandsApplication/ViewModels/LandViewModel.cs
+++ LandsApplication/LandsApplication/ViewModels/LandViewModel.cs
@@ -8,19 +8,49 @@
     public class LandViewModel : BaseViewModel
     {
         private ObservableCollection<Border> borders;
-
+        private ObservableCollection<Currency> currencies;
+        private ObservableCollection<Language> languages;
+        private Translations translations;
         #region Propperties
         public Land Land
         {
             get;
             set;
         }
+
         public ObservableCollection<Border> Borders
         {
             get => borders;
             set
             {
                 SetValue(ref this.borders, value);
+            }
+        }
+
+        public ObservableCollection<Currency> Currencies
+        {
+            get => currencies;
+            set
+            {
+                SetValue(ref this.currencies, value);
+            }
+        }
+
+        public ObservableCollection<Language> Languages
+        {
+            get => languages;
+            set
+            {
+                SetValue(ref this.languages, value);
+            }
+        }
+
+        public Translations Translations
+        {
+            get => translations;
+            set
+            {
+                SetValue(ref this.translations, value);
             }
         }
         #endregion
@@ -30,6 +60,9 @@
         {
             this.Land = land;
             LoadBorders();
+            this.currencies = new ObservableCollection<Currency>(this.Land.Currencies);
+            this.languages = new ObservableCollection<Language>(this.Land.Languages);
+            this.translations = this.Land.Translations;
         }
         #endregion
 
